b6dbf082cc9c43f185f67a6c88b8e926a2c35b28
root <gloria.kitili@student.moringaschool.com>
2023-05-18 17:02:50 +0300

    instance variable, getter ,setters

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 class Dog    
-    
+    #setter
 def name=(this_dogs_name)
     @this_dogs_name= name  = this_dogs_name
 end
-
+ #getter
 def name
     @this_dogs_name
 end
 
 end
 
-odie = Dog.new
-odie.name = "Odie"
+odie = Dog.new #instance
+odie.name = "Odie" #method 
 
 puts odie.name
